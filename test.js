dedf380eabb5786d093368c9040bbd5e6341202b
souravkumardubey <skdlp2000@gmail.com>
2023-01-24 23:56:43 +0530

    global methods

--- test.js
+++ test.js
@@ -1,2 +1,19 @@
-const name = "Sourav";
-console.log(name);
+/*
+const greet = (name) => {
+  console.log(`Hello, ${name}`);
+};
+
+greet("sourav");
+*/
+
+// ? Global Object
+// console.log(global);
+global.setTimeout((text = "in the timeout") => {
+  // ! not necessary to use global keyword
+  console.log(text);
+  clearInterval(interval);
+}, 3000);
+
+const interval = setInterval((text = "in the interval") => {
+  console.log(text);
+}, 1000);
